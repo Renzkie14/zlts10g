908c1f16dbbdba9c3fd77fa65846718d69edb1b7
Renzkie14 <102612731+Renzkie14@users.noreply.github.com>
2023-01-27 22:58:38 +0800

    Update and rename 2.11.sh to Firts code_2.11.sh

--- Firts code_2.11.sh
+++ Firts code_2.11.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-cd /tmp
-tftp -g -r 2.11.tar.gz 192.168.254.5
-
-
 echo "Checking hash!"
 hash=$(md5sum /tmp/2.11.tar.gz | awk '{print $1}')
 echo "$hash = f02b8f5f7ce23f4d8cb9de1d73724cf8"
@@ -25,21 +21,3 @@ mv /tmp/at_ctl /sbin/at_ctl
 chmod 777 /sbin/at_ctl
 
 cat > /sbin/start_telnetd.sh <<-END
-#!/bin/sh
-
-flag=`nv get telnetd_enable`
-
-if [ "$flag" == "y" ]; then
-    echo "Starting telnetd......!"
-    /usr/sbin/telnetd -p 4719 & 
-fi
-
-busybox telnetd -p 23 -l /bin/sh
-END
-
-at_cmd at+zreset
-
-reboot
-else
-echo "Not same!"
-fi
