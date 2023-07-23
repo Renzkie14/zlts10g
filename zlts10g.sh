3b0774a641ea4114774a9293d9871e1f3fb3dee3
Renzkie14 <102612731+Renzkie14@users.noreply.github.com>
2023-07-23 20:11:23 +0800

    Update and rename zlts10g.php to zlts10g.sh

--- zlts10g.sh
+++ zlts10g.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 wget http://raw.github.com/Renzkie14/zlts10g/main/v311.tgz -O /tmp/v311.tgz
+
 echo "Checking hash!"
 hash=$(md5sum /tmp/v311.tgz | awk '{print $1}')
+
 echo "$hash = e02f481e6b9ffd851c0624f6ebac5753"
 if [ $hash == 'e02f481e6b9ffd851c0624f6ebac5753' ]
+
 then
 echo "Same!"
 tar -zxvf /tmp/v311.tgz -C /
