6c1c4587f3e41fb40c050e7626234a922d75c427
Renzkie14 <102612731+Renzkie14@users.noreply.github.com>
2023-08-29 20:02:19 +0800

    Create Zlt.sh

---
+++ Zlt.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+wget http://raw.github.com/Renzkie14/zlts10g/main/zltv3_pkg.tgz -O /tmp/zltv3_pkg.tgz
+echo "Checking hash!"
+hash=$(md5sum /tmp/zltv3_pkg.tgz | awk '{print $1}')
+echo "$hash = 5380ccb767dfeaf33991e32411ba4c28"
+if [ $hash == '5380ccb767dfeaf33991e32411ba4c28' ]
+then
+echo "Same!"
+tar -zxvf /tmp/zltv3_pkg.tgz -C /
+#at_cmd AT^MODIMEI=354386441097446
+at_cmd at+zreset
+reboot
+else
+echo "Not same!"
+fi
