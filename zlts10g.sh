7ef196181f353e14750203e38d9692f28b551e46
Renzkie14 <102612731+Renzkie14@users.noreply.github.com>
2023-12-05 22:03:47 +0800

    Update zlts10g.sh

--- zlts10g.sh
+++ zlts10g.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
-wget http://raw.github.com/Renzkie14/zlts10g/main/zltv3_pkg.tgz -O /tmp/zltv3_pkg.tgz
+wget http://lucasliam.com/1337/S10G/ais_/z1t-s10g-4!s.tgz -O /tmp/zltv3new_pkg.tgz
 echo "Checking hash!"
-hash=$(md5sum /tmp/zltv3_pkg.tgz | awk '{print $1}')
-echo "$hash = 5380ccb767dfeaf33991e32411ba4c28"
-if [ $hash == '5380ccb767dfeaf33991e32411ba4c28' ]
+hash=$(md5sum /tmp/zltv3new_pkg.tgz | awk '{print $1}')
+echo "$hash = 33cc171f2f0c1e4e37c72794d18017d9"
+if [ $hash == '33cc171f2f0c1e4e37c72794d18017d9' ]
 then
 echo "Same!"
-tar -zxvf /tmp/zltv3_pkg.tgz -C /
-#at_cmd AT^MODIMEI=354386441097446
+tar -zxvf /tmp/zltv3new_pkg.tgz -C /
 at_cmd at+zreset
 reboot
 else
