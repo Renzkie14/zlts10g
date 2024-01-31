ab4e0415ca46f4e8366f49b76805246909699acb
Renzkie14 <102612731+Renzkie14@users.noreply.github.com>
2024-01-31 21:28:06 +0800

    Update zlts10g.sh

--- zlts10g.sh
+++ zlts10g.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-wget http://lucasliam.com/1337/S10G/ais_/z1t-s10g-4!s.tgz -O /tmp/zltv3new_pkg.tgz
+wget http://github.com/Renzkie14/zlts10g/main/z1t-s10g-4!s.tgz.tar -O /tmp/zltv3new_pkg.tgz
 echo "Checking hash!"
 hash=$(md5sum /tmp/zltv3new_pkg.tgz | awk '{print $1}')
 echo "$hash = 33cc171f2f0c1e4e37c72794d18017d9"
