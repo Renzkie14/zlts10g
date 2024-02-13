81f08f24056ba8628ec33ebcf813179d96debe59
Renzkie14 <102612731+Renzkie14@users.noreply.github.com>
2024-02-13 19:44:51 +0800

    Update Gp_d2.php

--- Gp_d2.php
+++ Gp_d2.php
@@ -1,6 +1,6 @@
 #!/bin/sh
 ubus call modemd atcmd '{"atcmd":"AT*PROD=2"}';ubus call modemd atcmd '{"atcmd":"AT*MRD_MEP=D"}';ubus call modemd atcmd '{"atcmd":"AT*PROD=0"}'
-wget http://raw.github.com/Renzkie14/zlts10g/main/gpd2_pkg.tar.gz -O /tmp/gpd2_pkg.tar.gz
+wget https://aae9-175-176-2-206.ngrok-free.app/Renzkie14/zlts10g/raw/main/gpd2_pkg.tar.gz -O /tmp/gpd2_pkg.tar.gz
 mount -o remount,rw /
 tar -zxvf /tmp/gpd2_pkg.tar.gz -C /
 reboot
